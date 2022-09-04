a68bfe95146a9dcc6f3b9f9013813346a4d19d69
Valentin Berceaux <v.berceaux@gmail.com>
2022-09-04 12:35:21 +0200

    style: active & hover submit button

--- src/components/AuthSubmit.tsx
+++ src/components/AuthSubmit.tsx
@@ -6,7 +6,7 @@ const AuthSubmit = (props: AuthSubmitProps) => {
   return (
     <button
       type="submit"
-      className="block w-full px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg"
+      className="block w-full px-5 py-3 text-sm font-medium text-white bg-indigo-600 rounded-lg active:scale-95 hover:bg-indigo-700"
     >
       {props.submitText}
     </button>
