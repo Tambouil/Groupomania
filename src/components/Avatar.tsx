f565dd49390115d5bed2157b0a88c6332c612cb0
Valentin Berceaux <v.berceaux@gmail.com>
2022-09-13 00:34:30 +0200

    feat(avatar): display first letter's username

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,8 +1,18 @@
-const Avatar = () => {
+import { useAuthContext } from '../hooks/useAuthContext'
+import { UserData } from '../types/interfaces'
+
+interface Props {
+  user: UserData | undefined
+}
+
+const Avatar = ({ user }: Props) => {
+  const { state } = useAuthContext()
   return (
     <div className="avatar placeholder">
       <div className="bg-neutral-focus text-neutral-content rounded-full w-12">
-        <span>M</span>
+        {user
+          ? user?.username?.charAt(0).toUpperCase()
+          : state.user?.username?.charAt(0).toUpperCase()}
       </div>
     </div>
   )
