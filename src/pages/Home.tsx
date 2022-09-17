6bdd6e2bb6a5a09f41e18fde83cc4f4e58115e65
Valentin Berceaux <v.berceaux@gmail.com>
2022-09-17 09:11:49 +0200

    refactor: remove unused imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,7 @@
 import { useEffect } from 'react'
-import { Navigate } from 'react-router-dom'
 import Feed from '../components/Feed'
 import Navbar from '../components/Navbar'
 import Posts from '../components/Posts'
-import { useAuthContext } from '../hooks/useAuthContext'
 import { usePostsContext } from '../hooks/usePostsContext'
 
 const Home = () => {
